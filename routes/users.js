f826730a2e00f07f2d1edede3293ceae0973bdef
Hadarth <hadar.schindler@gmail.com>
2017-10-02 18:42:51 +0100

    added debug script added colorful debuggers for server log added users.jade template

--- routes/users.js
+++ routes/users.js
@@ -3,22 +3,32 @@ var router = express.Router();
 var db  = require('../db');
 
 
-function getUserDB(username, done) {
-  // db.get().query('SELECT * FROM users WHERE username = ? LIMIT 1', [username], function(err, rows, fields) {
-  console.log('db:');
-  console.log(db);
-  db.get().query('SELECT * FROM users', [username], function(err, rows, fields) {
+function DBGetUsers(username) {
+  db.get().query('SELECT * FROM users', function(err, rows, fields) {
     if (err) throw err;
-    // console.log(rows[0]);
-    done(rows[0]);
+    console.log('\x1b[33m%s\x1b[0m', "QUERY: 'SELECT * FROM users'");
+    console.log('\x1b[32m%s',"RESULT: ", JSON.stringify(rows), "\x1b[0m");
+    console.log(`\x1b[31mNumber of rows:\x1b[35m ${rows.length} \x1b[0m\n`)
+    return JSON.stringify(rows)
+  });
+}
+
+function DBGetUser(username) {
+  const queryStr = `SELECT * FROM users WHERE username = \'${username}\' LIMIT 1`;
+  db.get().query(queryStr, function(err, rows, fields) {
+    if (err) throw err;
+    console.log('\x1b[33m%s\x1b[0m', `QUERY: ${queryStr}`);
+    console.log('\x1b[32m%s',"RESULT: ", JSON.stringify(rows), "\x1b[0m\n");
   });
 }
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
-  getUserDB('hadar', function(args) {console.log(args);});
-  res.render('index', { title: 'USERS' });
+  DBGetUsers();
+  DBGetUser('hadar');
+  DBGetUser('yair');
+  res.render('users', { title: 'THIS IS USER LIST', user: 'Hadar' });
 });
 
 module.exports = router;

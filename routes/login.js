000db64e7d41ae4113aa9a53eb9b00873c50570f
Hadarth <hadar.schindler@gmail.com>
2017-10-09 17:54:51 +0100

    fixed app pathes for deployment

--- routes/login.js
+++ routes/login.js
@@ -4,9 +4,7 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('Hello');
-  console.log(__dirname);
-  res.sendFile('/Users/schindler/Development/Salaryo/react-backend/public/login_page.html');
+  res.sendFile(path.join(__dirname, '../public/', 'login_page.html'));
 });
 
 module.exports = router;

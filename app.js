84848dd5094b8175906d5b4492caf934a8153b3b
Hadarth <hadar.schindler@gmail.com>
2017-10-24 23:17:42 +0100

    last working on google server

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var api = require('./routes/api');
 var login = require('./routes/login');
+// var reactApp = require('./routes/reactApp');
 
 var app = express();
 
@@ -22,10 +23,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/login', login);
+// app.use('/react', reactApp);
 // app.use('/users', users);
 // app.use('/api', api);
 
